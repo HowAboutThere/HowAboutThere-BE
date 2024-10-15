b7aae7f5393ef053464f3ea9ae976c4c8ba8727b
sehwaniii <ha9948s@naver.com>
2024-10-15 11:33:23 +0900

    CORS 설정

--- src/main/java/com/kosa2team/howaboutthere/trip/config/CorsConfig.java
+++ src/main/java/com/kosa2team/howaboutthere/trip/config/CorsConfig.java
@@ -10,8 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:5173", "www.hat-kosa.com","hat-kosa.com")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
+
