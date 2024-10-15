626938b844a6fea9199aaca1b8f87de40c4b4ef9
Sehwaniii <142206581+Sehwaniii@users.noreply.github.com>
2024-10-15 11:12:05 +0900

    CorsConfig.java 업데이트

--- src/main/java/com/kosa2team/howaboutthere/trip/config/CorsConfig.java
+++ src/main/java/com/kosa2team/howaboutthere/trip/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "www.hat-kosa.com","hat-kosa.com")
+                .allowedOriginPatterns("http://localhost:5173", "www.hat-kosa.com","hat-kosa.com")
                 .allowedMethods("GET", "POST")
                 .allowCredentials(true);
     }
